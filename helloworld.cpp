c5cf27a7a10cfa259dd8fcd308bbf04e0244f11c
Avi Sharma <avisharma5016@gmail.com>
2025-07-17 10:13:49 +0530

    Create helloworld.cpp

---
+++ helloworld.cpp
@@ -0,0 +1,19 @@
+// Name - Avi Sharma
+// PRN - 24070123025
+// Batch - EnTC - A1
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "hello!"<<endl;
+    cout << "First Program"<<endl;
+    return 0;
+}
+/*
+hello!
+First Program
+
+=== Code Execution Successful ===
+*/
+
